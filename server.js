142edd8c3a581b577442c8694bb564dbedbd07b0
kashu-02 <hituji_shun@icloud.com>
2020-06-30 23:27:38 +0900

    test commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const line = require('@line/bot-sdk');
 
@@ -28,3 +29,18 @@ function handleEvent(event) {
 }
 
 app.listen(3000);
+*/
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/g/', (req, res) => res.json({method: "こんにちは、getさん"}))
+  .post('/p/', (req, res) => res.json({method: "こんにちは、postさん"}))
+  .post("/hook/", (req, res) => res.json({ test: "hook" }))// 追加
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
