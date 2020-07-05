0ebc76659649c1f6947e3e9f7ffc97505f917b53
kashu-02 <hituji_shun@icloud.com>
2020-07-05 12:06:23 +0900

    modify client

--- server.js
+++ server.js
@@ -1,45 +1,12 @@
-/*
-const express = require('express');
-const line = require('@line/bot-sdk');
-
-const SECRET_KEY = process.env.TEST_SECRET_KEY;
-const ACCESS_TOKEN = process.env.TEST_SECRET_KEY;
-const config = {
-  channelAccessToken: ACCESS_TOKEN,
-  channelSecret: SECRET_KEY
-};
-
-const app = express();
-app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
-});
-
-const client = new line.Client(config);
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
-
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text
-  });
-}
-
-app.listen(3000);
-*/
-
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000;
 const line = require("@line/bot-sdk"); 
-const client = new line.Client(config);
 const config = {
   channelAccessToken: process.env.TEST_ACCESS_TOKEN,
   channelSecret: process.env.TEST_SECRET_KEY
 };
+const client = new line.Client(config);
 
 
 express()
