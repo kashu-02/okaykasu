228e2053cd6df1617796158df36569a31895e9a7
kashu-02 <hituji_shun@icloud.com>
2020-07-05 11:51:59 +0900

    add middleware

--- server.js
+++ server.js
@@ -33,14 +33,24 @@ app.listen(3000);
 
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+const line = require("@line/bot-sdk"); 
+const config = {
+  TEST_ChannelAccessToken: process.env.TEST_ACCESS_TOKEN,
+  TEST_ChannelSecret: process.env.TEST_SECRET_KEY
+};
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/g/', (req, res) => res.json({method: "こんにちは、getさん"}))
-  .post('/p/', (req, res) => res.json({method: "こんにちは、postさん"}))
-  .post("/hook/", (req, res) => res.json({ test: "hook" }))// 追加
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/g/", (req, res) => res.json({ method: "こんにちは、getさん" }))
+  .post("/p/", (req, res) => res.json({ method: "こんにちは、postさん" }))
+  .post("/hook/", line.middleware(config), (req, res) => lineBot(req, res)) 
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+function lineBot(req, res) {
+  res.json({ test: "hook" });
+  console.log("pass"); 
+}
