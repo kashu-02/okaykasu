c3809768cc9dd043863f18565f2aeae049d0f3ae
kashu-02 <hituji_shun@icloud.com>
2020-07-05 12:00:35 +0900

    replytest

--- server.js
+++ server.js
@@ -35,10 +35,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000;
 const line = require("@line/bot-sdk"); 
+const client = new line.Client(config);
 const config = {
   channelAccessToken: process.env.TEST_ACCESS_TOKEN,
   channelSecret: process.env.TEST_SECRET_KEY
 };
+
 
 express()
   .use(express.static(path.join(__dirname, "public")))
@@ -51,6 +53,22 @@ express()
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 function lineBot(req, res) {
-  res.json({ test: "hook" });
-  console.log("pass"); 
+  res.status(200).end();
+  const events = req.body.events;
+  const promises = [];
+  for (let i = 0, l = events.length; i < l; i++) {
+    const ev = events[i];
+    promises.push(
+      echoman(ev)
+    );
+  }
+  Promise.all(promises).then(console.log("pass"));
+}
+
+async function echoman(ev) {
+  const pro =  await client.getProfile(ev.source.userId);
+  return client.replyMessage(ev.replyToken, {
+    type: "text",
+    text: `${pro.displayName}さん、今「${ev.message.text}」って言いました？`
+  })
 }
