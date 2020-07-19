de4a63be62cbbce9431674a32a84f9a0b1124b11
kashu-02 <hituji_shun@icloud.com>
2020-07-19 23:39:36 +0900

    debug get chunkrange

--- routes/v1/lib/ChunkRangeHandler.js
+++ routes/v1/lib/ChunkRangeHandler.js
@@ -5,19 +5,22 @@ const router    = express.Router();
 
 router.get('/',function(req,res){
   ChunkRangeDB.findAll({ limit: 1 }).then(chunkrange => {
-    console.log(`chunkrange:${chunkrange.chunkrange}`)
+    console.log(`chunkrange:${chunkrange}`);
+    /*
     const chunkdate = new Date(chunkrange.date);
     const chunkyear = chunkdate.getFullYear();
     const chunkday = chunkdate.getDate();
     const chunkmonth = chunkdate.getMonth() + 1;
     const chunkweek = [ "日", "月", "火", "水", "木", "金", "土" ][chunkdate.getDay()] ;
+    
     res.json(JSON.stringify({
       year: chunkyear,
       month: chunkmonth,
       date: chunkday,
       day: chunkweek,
       range:chunkrange.chunkrange
-    }));
+    }));*/
+    res.json(JSON.stringify(chunkrange));
   });
 });
 
