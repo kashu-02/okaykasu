58fd10d182a9a3acf76844c2a415cff14783e346
kashu-02 <hituji_shun@icloud.com>
2020-07-22 23:22:43 +0900

    add nextstage

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/ChunkRange', require('./lib/ChunkRangeHandler.js'));
-
+router.use('/NextStageRange', require('./lib/NextStageHandler.js'));
 module.exports = router;
