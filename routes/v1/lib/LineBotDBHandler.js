f0bb3ebdfd145764b70b1cab1485347baa87e380
kashu-02 <hituji_shun@icloud.com>
2020-07-24 13:22:29 +0900

    LINE　DB処理

--- routes/v1/lib/LineBotDBHandler.js
+++ routes/v1/lib/LineBotDBHandler.js
@@ -2,6 +2,7 @@
 const LineBotDB = require('./LineBotDB.js');
 const express = require('express');
 const router = express.Router();
+const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 /**
