07a83d0066d884e12d579743ee162e746b56ac2a
kashu-02 <hituji_shun@icloud.com>
2020-07-28 00:35:06 +0900

    chunk delete

---
+++ schedule/DeleteChunkRange.js
@@ -0,0 +1,29 @@
+const moment = require('moment');
+  require('moment-timezone');
+  moment.tz.setDefault('Asia/Tokyo');
+  moment.locale('ja', {
+    weekdays: ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"],
+    weekdaysShort: ["日","月","火","水","木","金","土"],
+});
+const ChunkRangeDB = require('../routes/v1/lib/ChunkRangeDB.js');
+
+function DeleteChunkRange(){
+    const nowday = moment().format("YYYY-MM-DD");
+    ChunkRangeDB.findAndCountAll({
+        attributes: ['date', 'chunkrange'],
+        order: [
+          ['date', 'ASC']
+        ],
+        offset: 0,
+        limit: 1
+      }).then(chunkrange => {
+        const chunkday = moment(chunkrange.rows[0].date);
+        if(chunkday.isSameOrBefore(nowday)){
+            OkaykasuDB.destroy({
+                where: {
+                  date: chunkday.format("YYYY-MM-DD")
+                }
+              }).then(() => {}); 
+        };
+      });
+}
