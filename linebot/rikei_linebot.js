1ecbe276b75f0f4083e19a7d9a3c22918a5687a1
kashu-02 <hituji_shun@icloud.com>
2020-07-27 23:51:45 +0900

    line

--- linebot/rikei_linebot.js
+++ linebot/rikei_linebot.js
@@ -81,10 +81,12 @@ exports.rikei_lineBot = function (req, res) {
             ['id', 'ASC']
           ]
         }).then(okaykasu => {
-        for(let i = 0;i < okaykasu.length;i++){
-          client.replyMessage(ev.replyToken, JSON.parse(okaykasu[i].okaykasu));
-        }
-        });
+            let sendokaykasu = [];
+          for(let i = 0;i < okaykasu.length;i++){
+             sendokaykasu.push(JSON.parse(okaykasu[i].okaykasu));  
+          }
+          client.replyMessage(ev.replyToken, sendokaykasu);
+          });
       break;
       case 'ちゃんく':
       case 'チャンク':
@@ -218,7 +220,7 @@ exports.rikei_lineBot = function (req, res) {
       {
       type: "text",
       text: `${pro}さん、友だち追加ありがとうございます！\n\nおk粕は日々の連絡事項や課題を配信するbotです。\n\n主な機能は、下のメニューから送信でき、\n\n「おk粕」=配信した内容を取得\n「classroom」=最新10件のclassroomの投稿を取得\n「チャンク」=チャンクの範囲を取得\n\nといった感じです。`
-      },
+      }/*,
       {
         type: "text",
         text: `なお、LINEだけでなく、Webブラウザからも閲覧できます。LINEが入っていない端末やiPadで使えます。\nWEBブラウザから閲覧するには生徒であることを確認するため、麗明のGoogleアカウントでのログインが必要となります。`
@@ -226,7 +228,7 @@ exports.rikei_lineBot = function (req, res) {
       {
         type: "text",
         text: "https://okaykasu.herokuapp.com/"
-      }
+      }*/
     ])
    }
    
