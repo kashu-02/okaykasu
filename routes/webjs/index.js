a0c97c73f48880a3229770975ed48a7cc7bc308c
kashu-02 <hituji_shun@icloud.com>
2020-08-19 01:02:46 +0900

    json

--- routes/webjs/index.js
+++ routes/webjs/index.js
@@ -73,7 +73,7 @@ function okaykasuFlex(linemessage){
   let returnwebmessage = "";
   if(linemessage.contents.type == 'carousel'){
     for(let i = 0;i < linemessage.contents.contents.length;i++){
-        returnwebmessage += `<${linemessage.contents.contents[i].header.contents[0].text}> \n ${linemessage.contents.contents[i].body.contents[0].text} \n`
+        returnwebmessage += `<${linemessage.contents.contents[i].header.contents[0].text}> \n ${linemessage.contents.contents[i].body.contents[0].text} \n\n`
     }
   }
 return returnwebmessage;
