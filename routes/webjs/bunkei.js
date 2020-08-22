2c7e0ff1608d5eeacca780d5927cb6d8331b7d0b
kashu-02 <hituji_shun@icloud.com>
2020-08-23 03:28:43 +0900

    bunkei

---
+++ routes/webjs/bunkei.js
@@ -0,0 +1,31 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const ChunkRangeDB = require('../v1/lib/ChunkRangeDB');
+const NextStageRangeDB = require('../v1/lib/NextStageRangeDB');
+
+/* GET home page. */
+router.get('/bunkei', (req, res, next) => {
+  Promise.all([ChunkRangeDB.findAndCountAll({
+    attributes: ['date', 'chunkrange'],
+    order: [
+      ['date', 'ASC']
+    ],
+    offset: 0,
+    limit: 1
+  }),NextStageRangeDB.findAndCountAll({
+    attributes: ['date', 'nextstagerange'],
+    order: [
+      ['date', 'ASC']
+    ],
+    offset: 0,
+    limit: 1
+  })]).then(function(results){
+      res.render('bunkei', { 
+        chunkrange: results[0].rows[0],
+        nextstagerange: results[1].rows[0]
+    });
+  });
+});
+
+module.exports = router;
