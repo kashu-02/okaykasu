85bc410203fdf699f96537d19a4ab56fc668c533
kashu-02 <hituji_shun@icloud.com>
2021-03-13 01:59:22 +0900

    basic auth

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const auth = require('../auth/api_auth');
 const router = express.Router();
 
+router.use(auth)
 router.use('/ChunkRange', require('./lib/ChunkRangeHandler.js'));
 router.use('/NextStageRange', require('./lib/NextStageHandler.js'));
 router.use('/Okaykasu', require('./lib/OkaykasuHandler.js'));
