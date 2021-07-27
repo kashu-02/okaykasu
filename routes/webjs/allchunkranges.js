4b72ccf3aaa32c6620f8c4ed34590cc9c4f3192e
kashu-02 <hituji_shun@icloud.com>
2021-07-28 00:41:10 +0900

    all ranges

--- routes/webjs/allchunkranges.js
+++ routes/webjs/allchunkranges.js
@@ -2,8 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const ChunkRangeDB = require('../v1/lib/ChunkRangeDB');
-const NextStageRangeDB = require('../v1/lib/NextStageRangeDB');
-
+const moment = require('moment');
+  require('moment-timezone');
+  moment.tz.setDefault('Asia/Tokyo');
+  moment.locale('ja', {
+    weekdays: ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"],
+    weekdaysShort: ["日","月","火","水","木","金","土"],
+});
 /* GET home page. */
 router.get('/', (req, res, next) => {
   ChunkRangeDB.findAll({
@@ -11,10 +16,14 @@ router.get('/', (req, res, next) => {
     order: [
       ['date', 'ASC']
     ]
-  }).then(function(results){
-    console.log(JSON.stringify(results));
+  }).then(function (results) {
+    let ranges = results.map(result => [{
+      date: moment(result.date).format(MM月DD日(ddd)),
+      chunkrange: result.chunkrange
+    }])
+    console.log(JSON.stringify(ranges));
       res.render('allchunkranges', { 
-        allchunkranges: results
+        allchunkranges: ranges
     });
   });
 });
